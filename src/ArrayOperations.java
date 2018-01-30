5ecde09c33cd75e352e544a3215ebc5e616971c6
zer@kth.se <zer@kth.se>
2018-01-30 12:25:04 +0900

    Method for binary search and membership queries created. Closes #3

--- src/ArrayOperations.java
+++ src/ArrayOperations.java
@@ -7,12 +7,16 @@ public class ArrayOperations {
     for (int i = 0; i < a.length; i++) {
       System.out.println(a[i]);
     }
+    System.out.println(search(a, 1024));
+    System.out.println(search(a, 2024));
+    System.out.println(membership(a, 1024));
+    System.out.println(membership(a, 2024));
   }
 
   /**
   * Method that sorts an integer-array of arbitrary length using the algorithm
   * Selection Sort.
-  * @param A integer-array of arbitrary length 
+  * @param A integer-array of arbitrary length
   */
   public static void sort(int[] A) {
     if (A.length == 0) {
@@ -35,5 +39,53 @@ public class ArrayOperations {
       A[currMinIdx] = pivotValueOld;
     }
     return;
+  }
+
+  /**
+  * Method that searchs a sorted integer-array of arbitrary size for a value key
+  * using the binary search algorithm.
+  * @param A integer array of arbitrary length. Needs to be sorted.
+  * @param key integer value to be searched for in the sorted array A
+  * @return returns the index of the value key if it exists in the array, otherwise
+  * it returns -1.
+  */
+  public static int search(int[] A, int key) {
+    if (A.length == 0) {
+      throw new IllegalArgumentException("Array must not be empty");
+    }
+    int x;
+    int l = 1;
+    int r = A.length - 1;
+
+    do {
+      x = (l+r) / 2;
+      if (key < A[x]) {
+        r = x - 1;
+      } else {
+        l = x + 1;
+      }
+    } while (key != A[x] && l <= r);
+
+    if (key == A[x]) {
+      return x;
+    } else {
+      return -1;
+    }
+  }
+
+  /**
+  Method that evaluates however an integer value key is present in an integer array A.
+  Makes use of the search()-method.
+  * @param A integer array of arbitrary length. Needs to be sorted.
+  * @param key integer value to be evaluated for membership in the sorted array A.
+  * @return returns true if the value key is present in the array, and false if it isn't.
+  */
+  public static boolean membership(int[] A, int key) {
+    int index = search(A, key);
+    if (index > 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
